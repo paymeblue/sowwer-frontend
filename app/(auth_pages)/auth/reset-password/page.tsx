b450dd4351e77438d3f32034b543c1d7b8c0d4ec
Kaempy <campa.banj@gmail.com>
2023-08-23 16:09:08 +0100

    fixed

--- app/(auth_pages)/auth/reset-password/page.tsx
+++ app/(auth_pages)/auth/reset-password/page.tsx
@@ -1,10 +1,11 @@
-import { Metadata } from "next";
+"use client";
+// import { Metadata } from "next";
 import { useSearchParams } from "next/navigation";
 import PasswordPage from "../components/Password";
 
-export const metadata: Metadata = {
-  title: "Reset Password | Soower",
-};
+// export const metadata: Metadata = {
+//   title: "Reset Password | Soower",
+// };
 const ResetPassordPage = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
