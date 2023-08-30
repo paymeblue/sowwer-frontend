6112fcbf03ef1ad422b6f7decbc44cff02681048
Kaempy <campa.banj@gmail.com>
2023-08-30 13:45:38 +0100

    small

--- app/components/page-not-found.tsx
+++ app/components/page-not-found.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button, Result } from "antd";
+import Layout from "app/(landing_pages)/components/layout";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import notFound from "public/assets/images/page-not-found.png";
@@ -7,16 +8,20 @@ import notFound from "public/assets/images/page-not-found.png";
 const PageNotFound = () => {
   const router = useRouter();
   return (
-    <Result
-      status="404"
-      icon={<Image src={notFound} alt="page not found!" />}
-      title="Sorry, the page you visited does not exist."
-      extra={
-        <Button type="primary" onClick={() => router.push("/")}>
-          Back Home
-        </Button>
-      }
-    />
+    <Layout>
+      <section className="h-screen bg-grad">
+        <Result
+          status="404"
+          icon={<Image src={notFound} alt="page not found!" />}
+          title="Sorry, the page you visited does not exist."
+          extra={
+            <Button type="primary" onClick={() => router.push("/")}>
+              Back Home
+            </Button>
+          }
+        />
+      </section>
+    </Layout>
   );
 };
 
