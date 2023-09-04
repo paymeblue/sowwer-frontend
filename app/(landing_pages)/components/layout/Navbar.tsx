3629d18b248b1e887baeec88a14c13fa9861f3bf
Kaempy <campa.banj@gmail.com>
2023-09-04 19:34:16 +0100

    ui updates

--- app/(landing_pages)/components/layout/Navbar.tsx
+++ app/(landing_pages)/components/layout/Navbar.tsx
@@ -343,7 +343,7 @@ const Navbar = () => {
           <Button
             type="default"
             size="large"
-            className="mx-auto flex h-[45px] items-center justify-center border-black font-body text-sm  font-medium text-black laptop:p-5 "
+            className="mx-auto flex h-[45px] items-center justify-center border-black bg-transparent font-body text-sm  font-medium text-black laptop:p-5 "
             onClick={() => {
               router.prefetch("auth/signin/donor");
               router.push("auth/signin/donor");
