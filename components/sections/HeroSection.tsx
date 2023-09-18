81bb9f4df93ef3557e08e638685d3695e2611567
Victor Whyte <victordavidwhyte@gmail.com>
2023-09-18 19:35:01 +0100

    feat: add adnimation to hero section

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -2,17 +2,31 @@
 import { Button } from "@components/ui/button";
 import { Heart2 } from "react-iconly";
 import SectionContainer from "./SectionContainer";
+import { motion } from "framer-motion";
+import { DEFAULT_VIEWPORT, defaultVariant } from "variants";
 
 const Hero = () => {
   return (
     <SectionContainer>
       <section className="flex min-h-screen w-full items-center justify-center">
         <div className="flex flex-col items-center space-y-6">
-          <h1 className="text-center font-title text-[4rem] leading-[4.6rem] text-black">
+          <motion.h1
+            variants={defaultVariant({})}
+            initial="hidden"
+            whileInView="visible"
+            viewport={DEFAULT_VIEWPORT}
+            className="text-center font-title text-[4rem] leading-[4.6rem] text-black"
+          >
             Alone we can do so little; <br /> together we can do so much.
-          </h1>
-          <div className="flex flex-col items-center space-y-2">
-            <p className="text-center text-[14px] text-body-1">
+          </motion.h1>
+          <motion.div
+            variants={defaultVariant({ delay: 0.5 })}
+            initial="hidden"
+            whileInView="visible"
+            viewport={DEFAULT_VIEWPORT}
+            className="flex flex-col items-center"
+          >
+            <p className="mt-2 text-center text-[14px] text-body-1">
               "In all things I have shown you that by working hard in this way
               we must help the weak and remember the words <br /> of the Lord
               Jesus, how He himself said it is more blessed to give than to
@@ -21,13 +35,13 @@ const Hero = () => {
             <span className="text-center text-[13px] text-body-1">
               — Acts 20:35 (ESV)
             </span>
-          </div>
-          <Button>
-            <div className="flex items-center space-x-2">
-              <Heart2 set="bold" size={19} />
-              <span>Make a Donation</span>
-            </div>
-          </Button>
+            <Button className="mt-6">
+              <div className="flex items-center space-x-2">
+                <Heart2 set="bold" size={19} />
+                <span>Make a Donation</span>
+              </div>
+            </Button>
+          </motion.div>
         </div>
       </section>
     </SectionContainer>
---
+++ variants/index.ts
@@ -0,0 +1,66 @@
+interface Viewport {
+  once?: boolean;
+  margin?: string;
+  amount?: "some" | "all" | number;
+}
+
+export const ANIMATE_ONCE =
+  process.env.NODE_ENV === "development" ? false : true;
+export const DEFAULT_VIEWPORT: Viewport = {
+  once: ANIMATE_ONCE,
+  amount: "some",
+};
+
+interface IDefaultVariant {
+  duration?: number;
+  delay?: number;
+}
+
+export const defaultVariant = ({
+  delay = 0.2,
+  duration = 0.3,
+}: IDefaultVariant) => ({
+  hidden: {
+    y: 30,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay,
+      duration,
+    },
+  },
+});
+
+export const cardItemVariant = {
+  hidden: {
+    y: 20,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+    },
+  },
+};
+
+export const cardContainerVariant = {
+  hidden: {
+    y: 0,
+    opacity: 1,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0,
+      delay: 0.2,
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
+  },
+};
