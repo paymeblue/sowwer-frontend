d20ee0b15357146f77ebba4eb10f8820ed3d31fe
Victor Whyte <victordavidwhyte@gmail.com>
2023-10-17 12:44:01 +0100

    feat: captcha on contact us form feature

---
+++ app/api/contact-us/route.ts
@@ -0,0 +1,86 @@
+import { ContactUsValidation } from "lib/validations/contactUs";
+import { NextRequest, NextResponse } from "next/server";
+import * as z from "zod";
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const data = await req.formData();
+  const fields = ["fullName", "phoneNumber", "email", "message"];
+
+  const formDataValues: Partial<z.infer<typeof ContactUsValidation>> | any = {};
+  fields.forEach((field) => {
+    const value = data.get(field);
+    if (value !== null) {
+      formDataValues[field] = value as z.infer<
+        typeof ContactUsValidation
+      >[keyof z.infer<typeof ContactUsValidation>];
+    }
+  });
+
+  const ip = req.headers.get("CF-Connecting-IP");
+  const token = data.get("cf-turnstile-response");
+  const dataRes = ContactUsValidation.safeParse(formDataValues);
+
+  if (!dataRes.success) {
+    // Handle validation errors
+    return new NextResponse(dataRes.error as any, { status: 400 });
+  }
+
+  const { email, fullName, message, phoneNumber } = dataRes.data;
+
+  const body = JSON.stringify({
+    message,
+    email,
+    fullname: fullName,
+    type: "soower",
+    phone: phoneNumber,
+  });
+
+  const formData = new FormData();
+  formData.append("secret", process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY!);
+  formData.append("response", token as string);
+  formData.append("remoteip", ip as string);
+
+  // Validate the token by calling the siteverify API endpoint.
+  const url = process.env.CLOUDFLARE_TURNSTILE_VERIFY_API!;
+  const result = await fetch(url, {
+    body: formData,
+    method: "POST",
+  });
+
+  const outcome = await result.json();
+  console.log(outcome, "outcome");
+
+  if (outcome.success) {
+    // Now call the send the original body to the external API
+    const externalApiResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}contact-us`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body,
+      }
+    );
+
+    const response = await externalApiResponse.json();
+    console.log(response, "response");
+
+    if (externalApiResponse.ok) {
+      const result = response;
+      return NextResponse.json(result);
+    } else {
+      const status = externalApiResponse.status;
+      const errorResponse = response;
+      console.log("oh no", errorResponse.message);
+      return NextResponse.json(errorResponse.message, { status });
+    }
+  } else {
+    return NextResponse.json(
+      `Captcha token invalid!. Please refresh Captcha ${
+        !outcome.success ? outcome["error-codes"][0] : null
+      }`,
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- components/forms/ContactForm.tsx
+++ components/forms/ContactForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Fragment, useEffect, useState } from "react";
 import {
   Form,
   FormControl,
@@ -23,11 +24,14 @@ import {
 } from "@components/ui/select";
 import codes from "@lib/CountryCode";
 import { FixedSizeList as List } from "react-window";
-import { useEffect } from "react";
+import Script from "next/script";
+import { useToast } from "@components/ui/use-toast";
 
 const FList = List as any;
 
 const ContactForm = () => {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
   const form = useForm<z.infer<typeof ContactUsValidation>>({
     resolver: zodResolver(ContactUsValidation),
     defaultValues: {
@@ -39,9 +43,42 @@ const ContactForm = () => {
   });
   const selectedCode = form.watch("countryCode");
 
-  const onSubmit = async (values: z.infer<typeof ContactUsValidation>) => {
-    console.log("Submitted", { values });
-    alert("Your message has been sent successfully");
+  const onSubmit = async (
+    values: z.infer<typeof ContactUsValidation>,
+    e: any
+  ) => {
+    const formData = new FormData(e?.target);
+    const { email, fullName, message, countryCode, phoneNumber } = values;
+    formData.append("email", email);
+    formData.append("fullName", fullName);
+    formData.append("message", message);
+    formData.append("countryCode", countryCode as string);
+    formData.append("phoneNumber", phoneNumber as string);
+
+    try {
+      setIsLoading(true);
+      const res = await fetch("/api/contact-us", {
+        method: "POST",
+        body: formData,
+      });
+      const result = await res.json();
+      if (res.ok) {
+        toast({
+          title: "Contact message sent successfully",
+        });
+      } else {
+        throw new Error(result);
+      }
+    } catch (err: any) {
+      console.log({ err });
+      toast({
+        variant: "destructive",
+        title:
+          err?.message || "Unable to send message. Please try again later.",
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -51,129 +88,144 @@ const ContactForm = () => {
   }, [selectedCode, form]);
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex h-full w-full flex-col"
-      >
-        <div className="flex w-full flex-col justify-start gap-4">
-          <FormField
-            control={form.control}
-            name="fullName"
-            render={({ field }) => (
-              <FormItem className="">
-                <FormLabel required>Full name</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Enter your full name"
-                    type="text"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem className="">
-                <FormLabel required>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Enter email address"
-                    type="text"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="flex flex-col space-y-2">
-            <FormLabel>Phone number</FormLabel>
-
-            <div className="flex w-full items-end">
-              <FormField
-                control={form.control}
-                name="countryCode"
-                render={({ field }) => (
-                  <FormItem className="">
-                    <Select onValueChange={field.onChange}>
-                      <SelectTrigger className="w-[4rem] rounded-br-none rounded-tr-none bg-[#D9D9D9]">
-                        <SelectValue className="text-lg">
-                          {form.watch("countryCode")?.split(",")[0]}
-                        </SelectValue>
-                      </SelectTrigger>
-                      <SelectContent>
-                        <FList
-                          height={200}
-                          itemCount={codes.length}
-                          itemSize={40}
-                          width="100%"
-                        >
-                          {({ index, style }: any) => {
-                            const option = codes[index];
-                            const key = `${index}-${option.code}`;
-                            return (
-                              <div key={key} style={style}>
-                                <SelectItem
-                                  key={key}
-                                  value={`${option.flag},${option.code}`}
-                                  className="flex items-center"
-                                >
-                                  <span>{option.flag}</span>
-                                  <span className="ml-2 text-[.6rem] text-gray-500">
-                                    {option.code}
-                                  </span>
-                                </SelectItem>
-                              </div>
-                            );
-                          }}
-                        </FList>
-                      </SelectContent>
-                    </Select>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="phoneNumber"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormControl>
-                      <Input type="phone" {...field} />
-                    </FormControl>
-                    {/* <span>{form.watch("countryCode")?.split(",")[1]}</span> */}
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
-          <FormField
-            control={form.control}
-            name="message"
-            render={({ field }) => (
-              <FormItem className="">
-                <FormLabel required>Message</FormLabel>
-                <FormControl>
-                  <Textarea rows={6} {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <Button
-          type="submit"
-          variant="secondary"
-          className="ml-auto mt-10 w-fit"
+    <Fragment>
+      <Script
+        src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+        async={true}
+        defer={true}
+      />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex h-full w-full flex-col"
         >
-          Submit
-        </Button>
-      </form>
-    </Form>
+          <div className="flex w-full flex-col justify-start gap-4">
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel required>Full name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your full name"
+                      type="text"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel required>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter email address"
+                      type="text"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex flex-col space-y-2">
+              <FormLabel>Phone number</FormLabel>
+
+              <div className="flex w-full items-end">
+                <FormField
+                  control={form.control}
+                  name="countryCode"
+                  render={({ field }) => (
+                    <FormItem className="">
+                      <Select onValueChange={field.onChange}>
+                        <SelectTrigger className="w-[4rem] rounded-br-none rounded-tr-none bg-[#D9D9D9]">
+                          <SelectValue className="text-lg">
+                            {form.watch("countryCode")?.split(",")[0]}
+                          </SelectValue>
+                        </SelectTrigger>
+                        <SelectContent>
+                          <FList
+                            height={200}
+                            itemCount={codes.length}
+                            itemSize={40}
+                            width="100%"
+                          >
+                            {({ index, style }: any) => {
+                              const option = codes[index];
+                              const key = `${index}-${option.code}`;
+                              return (
+                                <div key={key} style={style}>
+                                  <SelectItem
+                                    key={key}
+                                    value={`${option.flag},${option.code}`}
+                                    className="flex items-center"
+                                  >
+                                    <span>{option.flag}</span>
+                                    <span className="ml-2 text-[.6rem] text-gray-500">
+                                      {option.code}
+                                    </span>
+                                  </SelectItem>
+                                </div>
+                              );
+                            }}
+                          </FList>
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormControl>
+                        <Input type="phone" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel required>Message</FormLabel>
+                  <FormControl>
+                    <Textarea rows={6} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div
+              className="cf-turnstile"
+              data-theme="light"
+              data-retry-interval={3000}
+              data-sitekey={
+                process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY
+              }
+            />
+          </div>
+          <Button
+            loading={isLoading}
+            type="submit"
+            variant="secondary"
+            className="ml-auto mt-10 w-fit"
+          >
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </Fragment>
   );
 };
 
