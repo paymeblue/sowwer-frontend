d98ecb141159a90e3da5f6a0596e7737d09ebacd
Victor Whyte <victordavidwhyte@gmail.com>
2025-05-05 08:58:46 +0100

    feat: add launch route

---
+++ app/(landing)/launch/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+const Page = () => {
+  return redirect(
+    "https://www.eventbrite.com/e/official-launch-of-soower-foundation-tickets-1320901013489?aff=oddtdtcreator"
+  );
+};
+
+export default Page;
